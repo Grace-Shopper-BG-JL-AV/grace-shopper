54bde336483758ba3f5191e5a972f88978fdbe3d
Betsy Groton <grotoned@gmail.com>
2020-09-22 13:19:37 -0400

    feat: AllProducts react component

--- client/routes.js
+++ client/routes.js
@@ -4,6 +4,7 @@ import {withRouter, Route, Switch} from 'react-router-dom'
 import PropTypes from 'prop-types'
 import {Login, Signup, UserHome} from './components'
 import {me} from './store'
+import {AllProducts} from './components/allProducts'
 
 /**
  * COMPONENT
@@ -19,6 +20,8 @@ class Routes extends Component {
     return (
       <Switch>
         {/* Routes placed here are available to all visitors */}
+        {/* connected all products onto the routes */}
+        <Route path="/allProducts" component={AllProducts} />
         <Route path="/login" component={Login} />
         <Route path="/signup" component={Signup} />
         {isLoggedIn && (
