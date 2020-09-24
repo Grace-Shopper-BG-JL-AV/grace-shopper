2c4c00e78dea84e7867334d5b546c2c64116b637
Betsy Groton <grotoned@gmail.com>
2020-09-23 23:50:28 -0400

    fix: edited redux tests

--- client/store/product.spec.js
+++ client/store/product.spec.js
@@ -1,11 +1,13 @@
 import {expect} from 'chai'
-import {fetchProducts, setProducts} from './product'
+import {fetchProducts, setProducts, SET_PRODUCTS} from './product'
 import axios from 'axios'
 import MockAdapter from 'axios-mock-adapter'
 import configureMockStore from 'redux-mock-store'
 import thunkMiddleware from 'redux-thunk'
 import {createStore} from 'redux'
-import appReducer from './index'
+
+import {rootReducer} from '../store'
+
 import {composeWithDevTools} from 'redux-devtools-extension'
 
 const middlewares = [thunkMiddleware]
@@ -13,19 +15,19 @@ const mockStore = configureMockStore(middlewares)
 
 describe('Redux Products', () => {
   const products = [
-    //will need to edit when I can see the models
-    {id: 1, name: 'dog costume', imageUrl: '/images/r2d2.png'},
-    {id: 2, name: 'another costume', imageUrl: '/images/walle.jpeg'}
+    {id: 1, name: 'dog costume', description: 'cute'},
+    {id: 2, name: 'another costume', description: 'so cute'}
   ]
 
   let store
   let mockAxios
 
-  const initialState = {products: []}
+  const initialState = {products: [], user: {}, product: {}, users: []}
 
   beforeEach(() => {
     mockAxios = new MockAdapter(axios)
     store = mockStore(initialState)
+    mockAxios.onGet('/api/products').replyOnce(200, products)
   })
 
   afterEach(() => {
@@ -41,36 +43,53 @@ describe('Redux Products', () => {
       })
     })
 
-    xit('fetchProducts thunk creator returns a thunk that GETs /api/products', async () => {
+    it('fetchProducts thunk creator returns a thunk that GETs /api/products', async () => {
       await store.dispatch(fetchProducts())
       const [getRequest] = mockAxios.history.get
       expect(getRequest).to.not.equal(undefined)
       expect(getRequest.url).to.equal('/api/products')
       const actions = store.getActions()
       expect(actions[0].type).to.equal('SET_PRODUCTS')
-      expect(actions[0].robots).to.deep.equal(products)
+      expect(actions[0].products).to.deep.equal(products)
     })
   })
 
+  //keep getting error: expect reducer in before hook to be a function. need to fix.
   describe('products reducer', () => {
     let testStore
     beforeEach(() => {
-      testStore = createStore(appReducer, composeWithDevTools)
+      testStore = createStore(rootReducer)
     })
 
     xit('returns the initial state by default', () => {
-      expect(store.getState().products).to.be.an('array')
+      expect(testStore.getState().products).to.be.an('array')
     })
 
-    xit('reduces on SET_PRODUCTS action', () => {
-      const action = {type: 'SET_PRODUCTS', products}
+    xit('handles SET_PRODUCTS as expected', () => {
+      const reducer = products(initialState, {
+        type: SET_PRODUCTS,
+        payload: {
+          data: [{id: 1, name: 'dog costume', description: 'cute'}]
+        }
+      })
 
-      const prevState = testStore.getState()
-      testStore.dispatch(action)
-      const newState = testStore.getState()
-
-      expect(newState.products).to.be.deep.equal(products)
-      expect(newState.products).to.not.be.equal(prevState.products)
+      expect(reducer).toEqual({
+        products: [{id: 1, name: 'dog costume', description: 'cute'}],
+        user: {},
+        product: {},
+        users: []
+      })
     })
+
+    // it('reduces on SET_PRODUCTS action', () => {
+    //   const action = {type: 'SET_PRODUCTS', products}
+
+    //   const prevState = testStore.getState()
+    //   testStore.dispatch(action)
+    //   const newState = testStore.getState()
+
+    //   expect(newState.products).to.be.deep.equal(products)
+    //   expect(newState.products).to.not.be.equal(prevState.products)
+    // })
   })
 })
