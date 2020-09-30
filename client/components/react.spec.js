0c0e0adffd59be7351be08a609565ba5f647be8c
Betsy Groton <grotoned@gmail.com>
2020-09-30 08:32:07 -0400

    fix: debugging specs

--- client/components/react.spec.js
+++ client/components/react.spec.js
@@ -1,26 +1,42 @@
 import {expect} from 'chai'
 import React from 'react'
-import enzyme, {shallow} from 'enzyme'
+import enzyme, {shallow, mount} from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
 import AllProducts from './allProducts'
+import {Provider} from 'react-redux'
+import configureStore from 'redux-mock-store'
 
 const adapter = new Adapter()
 enzyme.configure({adapter})
 
-//still fixing this one...
 describe('AllProducts', () => {
-  const prods = [
-    {id: 1, name: 'dog costume', description: 'cool'},
-    {id: 2, name: 'costume', description: 'a costume'}
-  ]
+  const mockState = {
+    prods: [
+      {id: 1, name: 'dog costume', description: 'cool'},
+      {id: 2, name: 'costume', description: 'a costume'}
+    ]
+  }
+  const mockStore = configureStore()
+  const store = mockStore(mockState)
 
-  let allProducts
-
+  let wrapper
   beforeEach(() => {
-    allProducts = shallow(<AllProducts products={prods} />)
+    wrapper = mount(
+      <Provider store={store}>
+        <AllProducts />
+      </Provider>
+    )
   })
 
   xit('renders the name and description', () => {
-    expect(allProducts.find('Product').length).to.be(2)
+    expect(AllProducts.find('Product').length).to.be(2)
+  })
+
+  describe('all products test', function() {
+    xit('renders description', function() {
+      wrapper = shallow(<AllProducts />)
+      const description = <p>cool</p>
+      expect(wrapper.contains(description)).to.equal(true)
+    })
   })
 })
